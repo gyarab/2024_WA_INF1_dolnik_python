b81a20b05dca35ff007eb2d482ca1154336273be
DolnikJan <jan.dolnik@student.gyarab.cz>
2024-11-28 11:04:34 +0100

    changes

--- test.py
+++ test.py
@@ -2,11 +2,12 @@
 def rotate_array(arr, n):
     if not isinstance(arr, list) or not isinstance(n, int):
         raise ValueError("Invalid arguments")
+    if not arr:
+        return arr
     if n >= 0:
-        return arr[-n:] + arr[:-n]
+        return arr[-n % len(arr):] + arr[:-n % len(arr)]
     else:
-        return arr[-n:] + arr[:-n]
-
+        return arr[-n % len(arr):] + arr[:-n % len(arr)]
 
 
 
