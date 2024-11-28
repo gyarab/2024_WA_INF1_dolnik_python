dd3b5dce49916599495f25065965a2896f81cbb2
DolnikJan <jan.dolnik@student.gyarab.cz>
2024-11-28 11:50:52 +0100

    breaks etc

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 
+
 def rotate_array(arr, n):
     if not isinstance(arr, list) or not isinstance(n, int):
         raise ValueError("Invalid arguments")
@@ -20,23 +21,24 @@ def split_into_threes(text):
         return [text[i:i+3] for i in range(0, len(text)-last_string_length, 3)] + [text[-last_string_length:]]
 
 
-def count_vowels_and_consonants(text):
-    if not isinstance(text, str):
-        raise ValueError("Invalid argument: text must be a string")
-    
-    vowels = ['a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ú', 'ů', 'ý', 'ě']
-    consonants = ['b', 'c', 'č', 'd', 'ď', 'f', 'g', 'h', 'ch', 'j', 'k', 'l', 'm', 'n', 'ň', 'p', 'r', 'ř', 's', 'š', 't', 'ť', 'v', 'z', 'ž']
-    vowel_count = 0
-    consonant_count = 0
-    
-    for char in text.lower():
-        if char.isalpha():
-            if char in vowels:
-                vowel_count += 1
-            elif char in consonants:
-                consonant_count += 1
-    
-    return {'vowels': vowel_count, 'consonants': consonant_count}
+class_duration = 45
+
+break_durations = [0, 5, 10, 20, 10, 10, 5, 5, 10, 10, 5, 5]
+
+def class_and_break_time(start_class, end_class):
+    if not isinstance(start_class, int) or not isinstance(end_class, int):
+        raise ValueError("Invalid arguments: start_class and end_class must be integers")
+    if start_class < 0 or start_class > 13 or end_class < 0 or end_class > 13 or start_class > end_class:
+        raise ValueError("Invalid arguments: start_class and end_class must be between 1 and 7 and start_class must be smaller or equal to end_class")
+
+    class_time = (end_class - start_class + 1) * class_duration
+    break_time = sum(break_durations[start_class-1:end_class-1])
+
+    return [class_time, break_time]
+
+
+                
+
 
 
 
