65e8793049c32139ad2098a64b059c749b0892ec
DolnikJan <jan.dolnik@student.gyarab.cz>
2025-01-30 12:10:41 +0100

    prace na hodine

--- content/admin.py
+++ content/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Article, Category
 
+
 # Register your models here.
 
-admin.site.register(Article)
 admin.site.register(Category)
+
+class ArticleAdmin(admin.ModelAdmin):   
+    list_display = ('title', 'published', 'category')
+    list_display_links = ('title', 'published')
+    list_filter = ('published', 'category')   
+    date_hierarchy = 'published'
+    search_fields = ('title', 'perex', 'text')
+
+admin.site.register(Article, ArticleAdmin)
