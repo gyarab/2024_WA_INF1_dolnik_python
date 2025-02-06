c073c2f2bd6e5419d4efeb82ea70c9ff76326d46
DolnikJan <jan.dolnik@student.gyarab.cz>
2025-02-06 12:06:09 +0100

    Práce v hodině

--- content/admin.py
+++ content/admin.py
@@ -2,15 +2,13 @@ from django.contrib import admin
 from .models import Article, Category
 
 
-# Register your models here.
-
-admin.site.register(Category)
-
-class ArticleAdmin(admin.ModelAdmin):   
-    list_display = ('title', 'published', 'category')
-    list_display_links = ('title', 'published')
-    list_filter = ('published', 'category')   
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    list_display_links = ['id', 'title']
+    #list_filter = ['category']
     date_hierarchy = 'published'
-    search_fields = ('title', 'perex', 'text')
+    search_fields = ['title', 'perex', 'text']
 
+# Register your models here.
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category)
--- content/models.py
+++ content/models.py
@@ -1,24 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=200)
 
     class Meta:
         verbose_name_plural = "Categories"
 
-
     def __str__(self):
         return self.name
-    
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=100)
-    perex = models.TextField(max_length=200)
+    title = models.CharField(max_length=200)
+    perex = models.TextField()
     text = models.TextField()
     published = models.DateTimeField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    #category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    categories = models.ManyToManyField(Category, related_name='articles')
 
     def __str__(self):
         return self.title
