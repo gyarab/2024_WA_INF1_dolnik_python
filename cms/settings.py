37a53b03cf10bbcf20b5bff9f1c3ebf8f5849d17
DolnikJan <jan.dolnik@student.gyarab.cz>
2025-03-27 10:36:57 +0100

    nevím už

--- cms/settings.py
+++ cms/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2$h#$zb(oaq%r*v4m27m2c+%5)4=3*-2zfcqavcu5l-18&&=vc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['dolnik.svs.gyarab.cz', '127.0.0.1']
 
@@ -116,11 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static/",
+    BASE_DIR / "static",
 ]
 
 # Default primary key field type
